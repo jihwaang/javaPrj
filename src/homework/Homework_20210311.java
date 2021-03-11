75a7081fe6276d3ecf5001983ac20ac598a438a8
jihwang <jihwaang@naver.com>
2021-03-11 14:20:22 +0900

    csv file reading homework refactored

--- src/homework/Homework_20210311.java
+++ src/homework/Homework_20210311.java
@@ -1,39 +1,32 @@
 package homework;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Homework_20210311 {
 	public static void main(String[] args) throws IOException {
 		FileInputStream fis = new FileInputStream("res/제주특별자치도_코로나현황_20201214.csv");
-		BufferedReader br = null;
-		String line;
-		String[] result = new String[] {"","",""};
-		br = new BufferedReader(new InputStreamReader(fis, "euc-kr"));
-		
-		int count = 0;
-		while((line = br.readLine()) != null) {
-			//첫번째 줄 처리
-				String[] eachLine = line.split(",");
-				for (int i = 0; i < result.length; i++) {
-					if(count == 1) {
-						result[i] += " : ";
-						eachLine[0] = eachLine[0].substring(0, 4) + "년 " + eachLine[0].substring(5, 7) + "월 " + eachLine[0].substring(8, 10) + "일";
-						if(!eachLine[2].contains("명"))
-							eachLine[2] += "명";
-					}
-					result[i] += eachLine[i];
-				}
-			count++;
-			//첫번째 바로 다음줄에서 반복문 중단
-			if(count == 2) break;
+		InputStreamReader isr = new InputStreamReader(fis, "euc-kr");
+		Scanner sc = new Scanner(isr);
+		String firstLine = sc.nextLine();
+		String[] firstLineResult = firstLine.split(",");
+		String eachLine;
+		String[] datas = null;
+		while(sc.hasNextLine()) {
+			eachLine = sc.nextLine();
+			datas = eachLine.split(",");
+			datas[0] = datas[0].substring(0,4)+"년 "
+			          +datas[0].substring(5,7)+"월 "
+			          +datas[0].substring(8,10)+"일"; 
+			break;
 		}
-		System.out.println("————————————————————————————————");
-		for(String str : result) System.out.println(str);
-		System.out.print("————————————————————————————————");
-		br.close();
+		if(datas !=null) 
+			for(int i = 0; i < 3; i++)
+				System.out.println(firstLineResult[i] + " : " + datas[i]);
+		sc.close();
 		fis.close();
 		
 		
