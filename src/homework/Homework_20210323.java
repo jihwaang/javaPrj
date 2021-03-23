6224c221ebbb50729ed48d5052923ef87d250868
jihwang <jihwaang@naver.com>
2021-03-23 20:36:44 +0900

    modified homework for today due to a bug with duplicates

--- src/homework/Homework_20210323.java
+++ src/homework/Homework_20210323.java
@@ -14,24 +14,12 @@ public class Homework_20210323 {
 		sort(lotto);
 		// 로또를 출력하자
 		print(lotto);
-		// 로또 중복 번호 처리
-		removeDuplication(random, lotto);
-		// 로또를 정렬하고
-		sort(lotto);
-		// 로또를 출력하자.
-		print(lotto);
-	}
-
-	private static void removeDuplication(Random random, int[] lotto) {
-		for(int i = 0; i < lotto.length; i++)
-			for(int j = 0; j < i; j++)
-				if(lotto[j] == lotto[i]) lotto[j] = random.nextInt(45)+1;
 	}
 
 	private static void sort(int[] lotto) {
-		for(int i = 0; i < lotto.length; i++) {
-			for(int j = 0; j < lotto.length; j++) {
-				if(lotto[i]<lotto[j]) {
+		for (int i = 0; i < lotto.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (lotto[i] < lotto[j]) {
 					int max = lotto[j];
 					lotto[j] = lotto[i];
 					lotto[i] = max;
@@ -41,21 +29,32 @@ public class Homework_20210323 {
 	}
 
 	private static void print(int[] lotto) {
-		for(int i = 0; i < lotto.length; i++) {
+		for (int i = 0; i < lotto.length; i++) {
 			System.out.print(lotto[i]);
-			if(i < lotto.length-1)
+			if (i < lotto.length - 1)
 				System.out.print(",");
-			else System.out.println();
+			else
+				System.out.println();
 		} // end for loop
-				
+
 	}
 
 	private static int[] get(Random random) {
 		int[] lotto = new int[6];
-		for(int i = 0; i < lotto.length; i++) {
-			lotto[i] = random.nextInt(45)+1;
+		for (int idx = 0; idx < lotto.length; idx++) {
+			int lottoNumber = random.nextInt(45) + 1;
+			if (!isDuplicated(lotto, idx, lottoNumber, random))
+				lotto[idx] = lottoNumber;
+			else
+				idx--;
 		} // end for loop
-		
 		return lotto;
+	}
+
+	private static boolean isDuplicated(int[] lotto, int idx, int lottoNumber, Random random) {
+		for (int i = 0; i < idx; i++)
+			if (lotto[i] == lottoNumber)
+				return true;
+		return false;
 	}
 }
